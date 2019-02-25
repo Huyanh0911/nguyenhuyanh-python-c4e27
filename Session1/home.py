67b2b24ed0a240653ed1bd4a19097c07ee1958d7
unknown <stephh9830@gmail.com>
2019-02-25 20:27:17 +0700

    hello anh em

---
+++ Session1/home.py
@@ -0,0 +1,29 @@
+# x= input ("Nhap ban kinh duong tron:")
+# x = int(x)
+# y = 3.14*x**2
+# print("Dien tich hinh tron la:",y)
+# -----------------------------------
+# a=input("Hay nhap nhiet do bang don vi Celcius:")
+# a=int(a)
+# b=a*1.8+32
+# print("%s(C)=%s(F)" %(int (a),int(b))) 
+# print(a,"(C) = ",
+# print(list(range(10)))
+# tong = 0
+# x=input(" nhap n: ")
+# x=int(x)
+# for v in range(1,x+1):
+#     tong = tong + v
+# print(tong)
+
+y=int(input("nhap nam sinh: "))
+if y<10:
+    print('baby')
+else:
+    if 10<y<16:
+        print('teen')
+    else:
+        print("adult")
+
+    
+
