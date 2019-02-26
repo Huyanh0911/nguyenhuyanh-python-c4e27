00cafbb0259689f443da251b7da933d528ed162b
unknown <stephh9830@gmail.com>
2019-02-26 22:22:54 +0700

    btvn

---
+++ Session1/homework/homework1.py
@@ -0,0 +1,73 @@
+# x = float(input("Nhap chieu cao cua ban(cm): "))
+# cv = x/100
+# y = float(input("Nhap can nang cua ban(kg): "))
+# BMI = y/cv**2
+# BMI=float(BMI)
+# print ("Chi so BMI cua ban la: ",BMI)
+# if BMI<16:
+#     print("Ban suy dinh duong cmnr :)")
+# elif 16<BMI<18.5:
+#     print("Ban hoi gay nha :>")
+# elif 18.5<BMI<25:
+#     print("Ban rat rat binh thuong")
+# elif 25<BMI<30:
+#     print("Ban hoi phung phinh")
+# else :
+#     print("Ban beo vailin")
+# --------------------------------------------------------
+# a = 1
+# x=input(" nhap n: ")
+# x=int(x)
+# for v in range(1,x+1):
+#     a = a * v
+# print(a)
+# -------------------------------------------------------------
+# print("Hello", end ="") 
+# print(",my name", end ="")
+# print("is B-max")
+# --------------------------------------------------------------
+# for i in range(20):
+#     print('.',end ='')
+# --------------------------------------------------------------
+# x=int(input("Nhap n: "))
+# for i in range(1,x+1):
+#     print(".",end="")
+# --------------------------------------------------------------
+# for i in range(1):
+#     print('x*'*4,end='')
+#     print('x')
+#--------------------------------------------------------------
+# x=int(input("Nhap n: "))
+# if x%2==0:
+#     a=int(x/2)
+#     for i in range(1):
+#         print('x*'*a,end = '')
+# else:
+#     b=int((x-1)/2)
+#     for i in range(1):
+#         print('x*'*b,end = '')
+#         print('x')
+#--------------------------------------------------------------
+# for a in range(3):
+#     for i in range(7):
+#         print('*',end='')
+#     for i in range(1):
+#         print()
+#--------------------------------------------------------------
+# x=int(input('nhap n: '))
+# y=int(input('nhap m: '))
+# for i in range(1,y+1):
+#     for i in range(1,x+1):
+#         print('*',end='')
+#     for i in range(1):
+#         print()
+#--------------------------------------------------------------
+
+
+    
+    
+
+
+
+
+    
